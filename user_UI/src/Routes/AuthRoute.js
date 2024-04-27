c6fa4fdb383da2c4d1bfecab9dc0d3d9e8bb8a49
Rahul Sadhu <rahuls96411@gmail.com>
2024-04-27 12:01:29 +0530

    updated

--- user_UI/src/Routes/AuthRoute.js
+++ user_UI/src/Routes/AuthRoute.js
@@ -6,6 +6,12 @@ import Signup from '../Component/Modal/Signup'
 import EventDetails from '../Component/Pages/EventDetails'
 import Event from '../Component/Pages/Event'
 import ProtectedPath from './ProtectedPath'
+import Home from '../Component/Pages/userDashboard/Home'
+import CreateEvent from '../Component/Pages/userDashboard/CreateEvent'
+import Rsvp from '../Component/Pages/userDashboard/Rsvp'
+import Notifications from '../Component/Pages/userDashboard/Notifications'
+import Layout from '../Component/Pages/userDashboard/Layout'
+import MyEvent from '../Component/Pages/userDashboard/MyEvent'
 const AuthRoute = () => {
   return (
    <>
@@ -15,10 +21,17 @@ const AuthRoute = () => {
     <Route path='/login' element={<Login/>}/>
     <Route path='/signup' element={<Signup/>}/>
     <Route path='/' element={<ProtectedPath component={EventDetails}/>}/>
-
-    <Route path='/' element={<ProtectedPath component={Event}/>}/>
+    {/* <Route path='/' element={<ProtectedPath component={Event}/>}/> */}
 
    </Routes>
+   <Route path="/user" element={<Layout />}>
+              <Route path="/user" element={<Home/>}/>
+              <Route path="/user/allevents" element={<Event />} />
+              <Route path="/user/myevents" element={<MyEvent />} />
+              <Route path="/user/rsvp" element={<Rsvp />} />
+              <Route path="/user/notifications" element={<Notifications />} />
+              <Route path="/user/createevent" element={<CreateEvent />} />
+    </Route>  
    </BrowserRouter>
    </>
   )
